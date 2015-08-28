b2d638584741f36db928ebd71c7f4edbf7f67093
frank <frankros91@gmail.com>
2015-08-28 14:59:10 -0400

    functional loader initial implementation complete

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('pre-test', function () {
 gulp.task('test', ['pre-test'], function (cb) {
   var mochaErr;
 
-  gulp.src('test/**/*.js')
+  gulp.src('lib/**/*.spec.js')
     .pipe(plumber())
     .pipe(mocha({reporter: 'spec'}))
     .on('error', function (err) {
